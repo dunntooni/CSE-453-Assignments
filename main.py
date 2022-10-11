a5e0c5d0064f0c5971f6854064722bbc48390c91
dunntooni <dun15014@byui.edu>
2022-10-11 15:32:55 -0600

    Created file

---
+++ main.py
@@ -0,0 +1,19 @@
+def displayMenu():
+    return
+
+def canonicalize():
+    return
+
+def isHomograph():
+    return
+
+def compareNonHomographs():
+    return
+
+def compareHomographs():
+    return
+
+def main():
+    return
+
+main()
