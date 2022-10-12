e9095c9896fbb0c7d36469e191a0ef0fb48d9884
dunntooni <dun15014@byui.edu>
2022-10-12 17:13:49 -0600

    Changed some file paths

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
-cannon_dir = "/users/joe/test/"
+canon_dir = "/users/joe/test/test.txt"
+
 
 def displayMenu():
     # comment
     return
 
+
 def canonicalize():
     return
+
 
 def isHomograph():
     return
 
+
 def compareNonHomographs():
     return
+
 
 def compareHomographs():
     return
 
+
 def main():
     return
+
 
 main()
