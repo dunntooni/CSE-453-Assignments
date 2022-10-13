a8fa90f599e00df93f76ec45d36cb196c28e230b
dunntooni <dun15014@byui.edu>
2022-10-13 13:18:08 -0600

    Created logic for displayMenu

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
+from collections import UserString
 import re
 
 canon_dir = "/users/joe/test/test.txt"
 
 
 def displayMenu():
-    # comment
-    return
+    userInput = ""
+    while(userInput != "M" and userInput != "A"):
+        userInput = input(
+            "Please input 'M' to manually input a file path or 'A' to automatically execute the test cases: ")
+        userInput = userInput.upper()
+    return userInput
 
 
 def canonicalize(url):
@@ -13,17 +18,43 @@ def canonicalize(url):
     return x
 
 
-def isHomograph():
+def isHomograph(f1, f2):
+    return True
+
+
+def compareNonHomographs(f1, f2):
     return
 
 
-def compareNonHomographs():
+def compareHomographs(f1, f2):
     return
 
 
-def compareHomographs():
-    return
-
-if __name__ == '__main__':
+def compareTwoFilePaths():
     print(canonicalize("./././.././test.txt"))
     print(canonicalize(".\.\././test.txt"))
+    file1 = input("Input the name of the first file: ")
+    file2 = input("Input the name of the second file: ")
+    file1 = canonicalize(file1)
+    file2 = canonicalize(file2)
+    print(file1)
+    print(file2)
+
+    homograph = isHomograph(file1, file2)
+
+    if homograph == True:  # This logic might not be correct. Feel free to revise if it isn't.
+        compareHomographs(file1, file2)
+    else:
+        compareNonHomographs(file1, file2)
+
+
+def executeTestCases():
+    return
+
+
+if __name__ == '__main__':
+    userInput = displayMenu()
+    if userInput == "A":
+        executeTestCases()
+    else:
+        compareTwoFilePaths()
