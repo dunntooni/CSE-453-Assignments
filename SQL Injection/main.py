c1d2a8317d62f67dce4f86243afb7d2869d10003
dunntooni <dun15014@byui.edu>
2022-10-18 15:22:14 -0600

    Added main.py for SQL injection

---
+++ SQL Injection/main.py
@@ -0,0 +1,29 @@
+def genQuery(username, password):
+    sql = ""
+    return sql
+
+def genQueryWeak(username, password):
+    sql = ""
+    return sql
+
+def genQueryStrong(username, password):
+    sql = ""
+    return sql
+
+def testValid():
+    pass
+
+def testTautology():
+    pass
+
+def testUnion():
+    pass
+
+def testAddState():
+    pass
+
+def testComment():
+    pass
+
+if __name__ == '__main__':
+    testValid()
