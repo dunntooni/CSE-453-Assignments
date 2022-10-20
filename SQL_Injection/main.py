9504c34b7f27f5d641f815cf47d8b8126739e43b
Paul A <andersonpaul238@ymail.com>
2022-10-20 13:22:36 -0600

    misc adjustments

--- SQL_Injection/main.py
+++ SQL_Injection/main.py
@@ -11,14 +11,14 @@ def genQueryStrong(username, password):
     return sql
 
 def testValid(queryGenerator):
-    print("TESTING VALID INPUTS")
-    print("********************")
+    print("\tTESTING VALID INPUTS")
+    print("\t********************")
 
     test_cases = [("ValidUsername", "Valid_Password")]
     for test in test_cases:
-        print(f"Username: {test[0]}  Password: {test[1]}")
+        print(f"\tUsername: {test[0]}  Password: {test[1]}")
         sql = queryGenerator(test[0], test[1])
-        print(f"SQL statement is:    {sql}")
+        print(f"\tSQL statement is:    {sql}")
 
 def testTautology():
     pass
@@ -33,4 +33,6 @@ def testComment():
     pass
 
 if __name__ == '__main__':
+    print("TESTING NO MITIGATION:")
+    print("**********************")
     testValid(genQuery)
