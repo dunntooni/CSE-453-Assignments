0399704fd78697d338804edd9eeee2e86d202fea
Paul A <andersonpaul238@ymail.com>
2022-10-21 10:53:08 -0600

    implemented strong mitigation, added comments

--- SQL_Injection/main.py
+++ SQL_Injection/main.py
@@ -4,19 +4,33 @@ import re
 def genQuery(username, password):
     return f"SELECT * FROM users WHERE userName = \'{username}\' AND userPassword = \'{password}\'"
 
-# 
+# This function runs user input through a blacklist of characters and removes characters that could prove problematic
+# We decided to allow the space character.
+# the Following characters are removed: ; - ' " = *
 def genQueryWeak(username, password):
     username = re.sub("[;\-\'\"=*]", "", username)
     password = re.sub("[;\-\'\"=*]", "", password)
     return f"SELECT * FROM users WHERE userName = \'{username}\' AND userPassword = \'{password}\'"
 
+# The strong mitigation will compare the inputed username and password against known usernames and passwords. If it doesn't
+# match any existing usernames or passwords, the whole input is rejected as bad input.
 def genQueryStrong(username, password):
-    usernameList = ["ValidUsername", "Erick", "ikeypoo"]
-    username = ["a","b", ...]
-    passwordList = []
+    #In a real system, these would be grabbed from a database in the background. For this purpose this is fine
+    knownUsernames = ["ValidUsername", "Erick", "ikeypoo"]
+    knownPasswords = ["Valid_Password", "coolEpicPassword97", "DankchickyNuggies_1"]
     
-    sql = ""
-    return sql
+    #If the username matches a known username, allow it to be plugged in to the sql statement 
+    sqlUsername = ""
+    if username in knownUsernames:
+        sqlUsername = username
+
+    #If the password matches a known password, allow it to be plugged in to the sql statement
+    sqlPassword = ""
+    if password in knownPasswords:
+        sqlPassword = password
+
+    #Plug in the accepted input to the SQL statement and make the query
+    return f"SELECT * FROM users WHERE userName = \'{sqlUsername}\' AND userPassword = \'{sqlPassword}\'"
 
 #This implementation requires the mitigation function be passed in as a parameter. This can be any mitigation function
 def testValid(queryGenerator):
@@ -27,7 +41,7 @@ def testValid(queryGenerator):
                   ("ikeypoo", "coolEpicPassword97"),
                   ("Erick", "DankchickyNuggies_1")]
     for test in test_cases:
-        print(f"\tTesting with inputs Username: {test[0]}  Password: {test[1]}")
+        print(f"\tTesting with inputs: Username = \"{test[0]}\"  Password = \"{test[1]}\"")
         sql = queryGenerator(test[0], test[1])
         print(f"\tSQL statement is:    {sql}")
     print("")
@@ -41,7 +55,7 @@ def testTautology(queryGenerator):
                   ("ikeypoo", "password' OR 'password lol' = 'password lol"), 
                   ("Erick", "asdf' OR 'blackTv_123' = 'blackTv123"),]
     for test in test_cases:
-        print(f"\tTesting with inputs Username: {test[0]}  Password: {test[1]}")
+        print(f"\tTesting with inputs: Username = \"{test[0]}\"  Password = \"{test[1]}\"")
         sql = queryGenerator(test[0], test[1])
         print(f"\tSQL statement is:    {sql}")
     print("")
@@ -55,7 +69,7 @@ def testUnion(queryGenerator):
                   ("ikeypoo", "gabbagoo' UNION SELECT * FROM authData;--"),
                   ("Erick", "main_admin' UNION SELECT * FROM users;--")]
     for test in test_cases:
-        print(f"\tTesting with inputs Username: {test[0]}  Password: {test[1]}")
+        print(f"\tTesting with inputs: Username = \"{test[0]}\" Password = \"{test[1]}\"")
         sql = queryGenerator(test[0], test[1])
         print(f"\tSQL statement is:    {sql}")
     print("")
@@ -69,7 +83,7 @@ def testAddState(queryGenerator):
                   ("regular_joe", "password'; DROP TABLE users;--"),
                   ("ikeypoo", "funnypasswordhaha'; INSERT INTO authData (userName, accessLevel) VALUES ('ikeypoo', 'Admin'); --")]
     for test in test_cases:
-        print(f"\tTesting with inputs Username: {test[0]}  Password: {test[1]}")
+        print(f"\tTesting with inputs:  Username = \"{test[0]}\" Password \"{test[1]}\"")
         sql = queryGenerator(test[0], test[1])
         print(f"\tSQL statement is:    {sql}")
     print("")
@@ -83,12 +97,13 @@ def testComment(queryGenerator):
                   ("BestUser';--", "You've been hacked!"),
                   ("Erick';--", "Sucks to be you XD")]
     for test in test_cases:
-        print(f"\tTesting with inputs Username: {test[0]}  Password: {test[1]}")
+        print(f"\tTesting with inputs: Username = \"{test[0]}\"  Password = \"{test[1]}\"")
         sql = queryGenerator(test[0], test[1])
         print(f"\tSQL statement is:    {sql}")
     print("")
 
 if __name__ == '__main__':
+    #For these inputs, the input should be plugged into the SQL query as is, allowing exploitation.
     print("TESTING NO MITIGATION:")
     print("*"*22 +"\n")
     testValid(genQuery)
@@ -97,6 +112,7 @@ if __name__ == '__main__':
     testAddState(genQuery)
     testComment(genQuery)
 
+    #For this round of testing, Weak mitigation should remove problematic characters from input and put remaining characters in query
     print("TESTING WEAK MITIGATION:")
     print("************************\n")
     testValid(genQueryWeak)
@@ -105,6 +121,7 @@ if __name__ == '__main__':
     testAddState(genQueryWeak)
     testComment(genQueryWeak)
 
+    #For this round of testing, strong mitigation compares user input to known acceptable input and disallow unknown input 
     print("TESTING STRONG MITIGATION:")
     print("**************************\n")
     testValid(genQueryStrong)
